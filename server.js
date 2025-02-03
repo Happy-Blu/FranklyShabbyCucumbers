4dad0978416812159e22bb297382b97346097a57
Happy_Blu <hazeliabell@gmail.com>
2025-02-03 22:26:56 +0000

    pleaseeeeee

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ if (isBun) {
   ;({ serveStatic } = await import('jsr:@hono/hono/deno'))
 }
 
+//i dunno
+let app = new Hono()
+app.get('/now', (c) => c.text(Date.now()))
+let startTime = Date.now()
+app.get('/started', (c) => c.text(startTime))
+app.get('/now', (c) => c.text(Date.now()))
+app.get('/online', (c) => c.text(`Online for ${((Date.now() - startTime)/1000).toFixed(0)}s`))
+
 // serve all files in the ./web subfolder
 app.use('/*', serveStatic({ root: './pages' }))
 app.get('*', serveStatic({ path: './pages/my404.html'}))
